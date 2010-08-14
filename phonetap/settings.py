0ae3f791e4475e2b7b1ccea1b064d868b515bd99
bmorris <ben@bnmrrs.com>
2010-08-14 17:06:06 -0400

    Added main to the list of installed apps

--- phonetap/settings.py
+++ phonetap/settings.py
@@ -71,12 +71,10 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     #'django.contrib.sessions',
     'django.contrib.sites',
+	'phonetap.main'
 )
 
 ROOT_PATH = os.path.dirname(__file__)
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or
-    # "C:/www/django/templates".  Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
     ROOT_PATH + '/templates',
 )
