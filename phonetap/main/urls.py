fcecea8523009e6bb18e51f4065b2a56ebd2125d
bmorris <ben@bnmrrs.com>
2010-08-14 17:45:26 -0400

    Changed naming of urls

--- phonetap/main/urls.py
+++ phonetap/main/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('phonetap.main.views',
-    url(r'^$', 'homepage', name="PhoneTap-main-homepage")
+    url(r'^$', 'homepage', name="phonetap-main-homepage")
 )
