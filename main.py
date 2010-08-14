8599d4a1a9cf1558e4b81ac2104a61e7637408f1
bmorris <ben@bnmrrs.com>
2010-08-14 17:14:02 -0400

    Django now works correctly

--- main.py
+++ main.py
@@ -14,42 +14,38 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import logging, os
+import logging, os, sys
 
 # Google App Engine imports.
 from google.appengine.ext.webapp import util
 
-# Force Django to reload its settings.
-from django.conf import settings
-settings._target = None
+# # Remove the standard version of Django.
+for k in [k for k in sys.modules if k.startswith('django')]:
+    del sys.modules[k]
 
-# Must set this env var before importing any part of Django
-# 'project' is the name of the project created with django-admin.py
+# # Force sys.path to have our own directory first, in case we want to import
+# # from it.
+BASE_PATH = os.path.dirname(__file__)
+sys.path.insert(0, os.path.abspath(BASE_PATH))
+for path, dirs, files in os.walk(os.path.join(BASE_PATH, 'deps')):
+    for dir in dirs:
+        sys.path.insert(0, os.path.join(path, dir))
+sys.path.insert(0, os.path.join(BASE_PATH, 'PhoneTap'))
+
+# Must set this env var *before* importing any part of Django
 os.environ['DJANGO_SETTINGS_MODULE'] = 'phonetap.settings'
 
-import logging
 import django.core.handlers.wsgi
 import django.core.signals
-import django.db
-import django.dispatch.dispatcher
 
 def log_exception(*args, **kwds):
-    logging.exception('Exception in request:')
+   logging.exception('Exception in request:')
 
 # Log errors.
-django.dispatch.dispatcher.connect(
-    log_exception, django.core.signals.got_request_exception)
-
-# Unregister the rollback event handler.
-django.dispatch.dispatcher.disconnect(
-    django.db._rollback_on_exception,
-    django.core.signals.got_request_exception)
+django.core.signals.got_request_exception.connect(log_exception)
 
 def main():
-    # Create a Django application for WSGI.
     application = django.core.handlers.wsgi.WSGIHandler()
-
-    # Run the WSGI CGI handler with that application.
     util.run_wsgi_app(application)
 
 if __name__ == '__main__':
