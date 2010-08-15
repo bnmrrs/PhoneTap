4ec9ce1c2476618d46eb2a138c0fee70cdcde7e9
bmorris <ben@bnmrrs.com>
2010-08-14 20:52:47 -0400

    Adding the make_call view def

--- phonetap/main/views.py
+++ phonetap/main/views.py
@@ -1,4 +1,33 @@
+import json
+import phonetap.twiliosimple
+
+from phonetap.main.forms import CallForm
+
+from django.http import HttpResponse
+from django.core import serializers
 from django.shortcuts import render_to_response
 
 def homepage(request):
-	return render_to_response('index.html', {})
+	form = CallForm()
+	
+	return render_to_response('index.html', {
+		'form': form,
+	})
+	
+def make_call(request):
+	if request.is_ajax():
+		form = CallForm(request.POST)
+		
+		if form.is_valid():
+			response = json.dumps({'success':True})
+		else:
+			response_dict = {
+				'success': False,
+				'errors': form.errors
+			}
+			
+			response = json.dumps(response_dict)
+		
+		return HttpResponse(response, 'application/javascript')
+	else:
+		return HttpResponse(status=400)
