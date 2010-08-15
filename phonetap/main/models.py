b601204e626fcc0e67590ff7ac340e5a89bf35d8
bmorris <ben@bnmrrs.com>
2010-08-15 00:28:20 -0400

    Added datastore

--- phonetap/main/models.py
+++ phonetap/main/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+from google.appengine.ext import db
 
-# Create your models here.
+class Call(db.Model):
+	caller_number = db.StringProperty(required=True)
+	callee_number = db.StringProperty(required=True)
+	caller_email = db.EmailProperty(required=True)
+	call_sid = db.StringProperty()
+	recording_url = db.StringProperty()
+	current_status = db.StringProperty()
+	start_time = db.DateTimeProperty(auto_now_add=True)
+	end_time = db.DateTimeProperty()
+	recording_duration = db.IntegerProperty()
--- phonetap/main/views.py
+++ phonetap/main/views.py
@@ -2,6 +2,9 @@ from django.utils import simplejson as json
 from twiliosimple import Twilio, Utils
 
 from phonetap.main.forms import CallForm
+from phonetap.main.models import Call
+
+from google.appengine.ext import db
 
 from django.http import HttpResponse
 from django.core import serializers
@@ -28,8 +31,18 @@ def make_call(request):
 			twilio = Twilio(settings.TWILIO_ACCOUNT_SID, \
 				settings.TWILIO_ACCOUNT_TOKEN)
 				
-			call = twilio.call(settings.TWILIO_SANDBOX_NUM, \
+			twilio_response = twilio.call(settings.TWILIO_SANDBOX_NUM, \
 				form.cleaned_data['caller_num'], callback)
+				
+			call = Call(
+				call_sid=twilio_response.call_sid,
+				caller_number=form.cleaned_data['caller_num'],
+				callee_number=form.cleaned_data['callee_num'],
+				caller_email=form.cleaned_data['caller_email'],
+				current_status='Dialing'
+			)
+			
+			call.put()
 			
 			response = json.dumps({'success':True})
 		else:
@@ -46,13 +59,26 @@ def make_call(request):
 		
 def outgoing_callback(request):
 	if is_valid_twilio_request(request):
+		q = db.GqlQuery('SELECT * FROM Call ' +
+						'WHERE call_sid = :1 ',
+						request.POST['CallSid'])
+		call = q.get()
+		
+		if not call:
+			raise Http404
+			
+		call.current_status = 'In Progress'
+		call.put()
+							
 		callback = request.build_absolute_uri(
 			reverse('phonetap-main-outgoing_recording')
-			)
+		)
 	
 		return render_to_response('outgoing_callback.html', {
-			'callback': callback
+			'callback': callback,
+			'number': call.callee_number
 		})
+		
 	else:
 		return HttpResponse(status=400)
 	
