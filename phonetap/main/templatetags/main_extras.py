ee730091b95c0d68ca16ef5ffd5f3a2710a94693
bmorris <ben@bnmrrs.com>
2010-08-15 12:07:37 -0400

    Added some templates

--- phonetap/main/templatetags/main_extras.py
+++ phonetap/main/templatetags/main_extras.py
@@ -4,5 +4,25 @@ def tel(value):
   value = str(value)
   return '(%s) %s-%s' % (value[0:3], value[4:7], value[8:12])
 
+def duration(duration):
+	if not duration:
+		return 'No duration'
+	else:
+		return value
+	
+def listen_to_recording(recording_url):
+	if not recording_url:
+		return 'No recording was found'
+	
+def recording_download(recording_url):
+	if not recording_url:
+		return 'No recording was found'
+	else:
+		return '<a href="%s">Recording</a>' % (recording_url)
+
 register = template.Library()
+
 register.filter('tel', tel)
+register.filter('duration', duration)
+register.filter('listen_to_recording', listen_to_recording)
+register.filter('recording_download', recording_download)
