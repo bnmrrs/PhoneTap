44816c4d7a98f06720673369ffa9d7716cb6efbd
bmorris <ben@bnmrrs.com>
2010-08-14 21:02:51 -0400

    Changing number to num on the form

--- phonetap/main/forms.py
+++ phonetap/main/forms.py
@@ -2,6 +2,6 @@ from django import forms
 from django.contrib.localflavor.us.forms import USPhoneNumberField
 
 class CallForm(forms.Form):
-	caller_number = USPhoneNumberField()
-	callee_number = USPhoneNumberField()
+	caller_num = USPhoneNumberField()
+	callee_num = USPhoneNumberField()
 	caller_email = forms.EmailField()
