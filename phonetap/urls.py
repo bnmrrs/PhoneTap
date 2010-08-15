7680d1be0738c000ab53e38b118787baff481846
bmorris <ben@bnmrrs.com>
2010-08-14 20:52:06 -0400

    Added the make_call url

--- phonetap/urls.py
+++ phonetap/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-	(r'^$', include('phonetap.main.urls')),
+	url(r'^make_call$', 'phonetap.main.views.make_call', name="phonetap-main-make_call"),
+    url(r'^$', 'phonetap.main.views.homepage', name="phonetap-main-homepage"),
+	#(r'^$', include('phonetap.main.urls')),
 )
